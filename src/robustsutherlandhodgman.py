7798c1bd50364ac68d9507ce184aece8cba0b233
ALIEN <matthias.moulin@gmail.com>
2015-10-30 15:44:27 +0100

    Update

--- src/robustsutherlandhodgman.py
+++ src/robustsutherlandhodgman.py
@@ -17,19 +17,23 @@ def plot_clip3D_AABB(p_vs, pmin, pmax):
     plt.plot_line3D(p_vs, color='r')
     plt.plot_AABB(pmin, pmax)
     result = clip3D_AABB(p_vs, pmin, pmax, step=True)
-    print (area3D(result, n))
+    print(result)
+    print(area3D(result, n))
     plt.plot_line3D(result, color='g')
 
 def clip3D_AABB(p_vs, pmin, pmax, step=False):
     for a in range(3):
        p_vs = clip3D_plane(p_vs, 1.0, a, pmin)
+       if step: print(p_vs)
        p_vs = clip3D_plane(p_vs, -1.0, a, pmax)
+       if step: print(p_vs)
     return p_vs
     
 def clip3D_plane(p_vs, s, a, c_v):
     new_p_vs = []
 
     nb_p_vs = len(p_vs)
+    if (nb_p_vs <= 1):  return []
     for j in range(nb_p_vs):
         p_v1 = p_vs[(j+nb_p_vs-1) % nb_p_vs]
         p_v2 = p_vs[j]
