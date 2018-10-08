f430d5d9b865356e4394c396b6faad56d09cbc17
Matthias Moulin <matthias.moulin@gmail.com>
2018-10-08 20:25:24 +0200

    Update sutherlandhodgman.py

--- src/sutherlandhodgman.py
+++ src/sutherlandhodgman.py
@@ -30,12 +30,12 @@ def clip2D_AABB(p_vs, pMin, pMax, step=False):
 
 def clip2D(p_vs, c_vs):
     new_p_vs = p_vs
-    
+
     nb_c_vs = len(c_vs)
     for i in range(nb_c_vs):
         c_v1 = c_vs[(i+nb_c_vs-1) % nb_c_vs]
         c_v2 = c_vs[i]
-        
+
         old_p_vs = new_p_vs
         new_p_vs = []
 
@@ -43,7 +43,7 @@ def clip2D(p_vs, c_vs):
         for j in range(nb_p_vs):
             p_v1 = old_p_vs[(j+nb_p_vs-1) % nb_p_vs]
             p_v2 = old_p_vs[j]
-            
+
             #Line segment clipping
             if inside2D(c_v1, c_v2, p_v2):
                 if not inside2D(c_v1, c_v2, p_v1): 
@@ -75,24 +75,24 @@ def clip3D_AABB(p_vs, pMin, pMax, step=False):
     c_vs = [array(v) for v in c_vs]
     if (step): print(p_vs)
     p_vs = clip3D_AABP(p_vs, c_vs, a0=2, a1=0)
-    
+
     sort_vertices(p_vs, a0=0, a1=1)
     c_vs = [[pMax[0], pMin[1], 0.0],[pMax[0], pMax[1], 0.0],[pMin[0], pMax[1], 0.0],[pMin[0], pMin[1], 0.0]]
     c_vs = [array(v) for v in c_vs]
     if (step): print(p_vs) 
     p_vs = clip3D_AABP(p_vs, c_vs, a0=0, a1=1)
-    
+
     if (step): print(p_vs)
     return p_vs
-    
+
 def clip3D_AABP(p_vs, c_vs, a0=0, a1=1):
     new_p_vs = p_vs
-    
+
     nb_c_vs = len(c_vs)
     for i in range(nb_c_vs):
         c_v1 = c_vs[(i+nb_c_vs-1) % nb_c_vs]
         c_v2 = c_vs[i]
-        
+
         old_p_vs = new_p_vs
         new_p_vs = []
 
@@ -100,12 +100,12 @@ def clip3D_AABP(p_vs, c_vs, a0=0, a1=1):
         for j in range(nb_p_vs):
             p_v1 = old_p_vs[(j+nb_p_vs-1) % nb_p_vs]
             p_v2 = old_p_vs[j]
-            
+
             if inside3D(c_v1, c_v2, p_v2, a0, a1):
                 if not inside3D(c_v1, c_v2, p_v1, a0, a1):
                     new_p_vs.append(intersect3D(c_v1, c_v2, p_v1, p_v2, a0, a1))
                 new_p_vs.append(p_v2)
             elif inside3D(c_v1, c_v2, p_v1, a0, a1):
                 new_p_vs.append(intersect3D(c_v1, c_v2, p_v1, p_v2, a0, a1))
-   
+
     return new_p_vs
